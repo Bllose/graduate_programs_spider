b723f11df63e9e73a9a690fa8b5424f4bf56e550
Bllose <bllose2018@gmail.com>
2020-09-20 14:44:56 +0800

    更新入口说明

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 
-if 'abc'.__contains__('d'):
-    print("YES!")
-elif '\n\n或\n'.__contains__('或'):
-    print("YES YES!")
-else:
-    print("no!")
+#Entry : general.programs.spider.Get_All_Programs.py
+
